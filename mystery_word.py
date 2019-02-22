84aa8bceff34e6f01dfe774a20b4b5f8eaa51fdd
Craig Brunengraber <brunengraber@hotmail.com>
2019-02-22 12:19:19 -0500

    started coding

--- mystery_word.py
+++ mystery_word.py
@@ -2,16 +2,16 @@
 # print("Please select your level of difficulty: easy, medium, hard")
 
 import string
-
-#tell the computer that words is word.txt - use with open so it will close the file when its done reading it
+import random
 
 
 def normalize_text(text):
     """Takes text and removes punctuation and replaces whitespace with normal spaces- compressing single spaces"""
     text = text.lower()
-    valid_chars = string.ascii_letters + string.whitespace + string.digits # check for valid chars add them to new var
+    valid_chars = string.ascii_letters + string.whitespace + \
+        string.digits  # check for valid chars add them to new var
 
-    #remove punctuation
+    # remove punctuation
     new_text = ""
     for char in text:
         if char in valid_chars:
@@ -20,7 +20,44 @@ def normalize_text(text):
     text = text.replace("\n", " ")
     return text
 
-with open('words.txt') as f: 
+# tell the computer that words is word.txt - use with open so it will close the file when its done reading it
+
+
+with open('words.txt') as f:
     words = f.read()
 
-print(words)
+# print(words)
+
+# user must enter level of difficulty
+level_of_difficulty = input("easy", "medium", "hard")
+
+#turn this to a while not loop
+
+while level_of_difficulty == (input):
+
+    if level_of_difficulty != (input):
+        return(print("You must enter easy, medium, hard, or just go play something else"))
+
+    if level_of_difficulty == "easy":
+        # program needs to slect proper list
+        # program print the word/spaces
+
+    elif level_of_difficulty == "medium":
+     # program needs to slect proper list
+     # program print the word/spaces
+
+    elif level_of_difficulty == "hard":
+        # program needs to slect proper list
+        # program print the word/spaces
+
+
+def level_of_difficulty(user_input="Welcome to word guess, please enter your level of difficulty: easy, medium, or hard")
+
+
+# create a for loop with three seperate if statements
+easy = []
+medium = []
+hard = []
+
+
+print("Great, you selected " + level_of_difficulty)
