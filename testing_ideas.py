70dff21f0faa8dfe78b5532539f9f2aa807c0068
Craig Brunengraber <brunengraber@hotmail.com>
2019-02-25 17:44:33 -0500

    building from scratch

--- testing_ideas.py
+++ testing_ideas.py
@@ -1,6 +1,11 @@
 import random
 import string
 
+
+#get random words into game
+def auto_selected_word(words):          
+    return random.choice(words).lower()
+
 def normalize_text(text):
     """Takes text and removes punctuation and replaces whitespace with normal spaces- compressing single spaces"""
     text = text.lower()
@@ -16,8 +21,25 @@ def normalize_text(text):
     text = text.replace("\n", " ")
     return text
 
+def difficulty(words, difficulty):  # IF STATEMENT - CONDITION IS MET THAN -----X HAPPENS, NOT A REPEATING LOOP
+    if difficulty = "easy": 
+        # select words from list 4-6 letters       
+    if difficulty = "normal":
+        # select words from list 6-8 letters
+    if difficulty = "hard":
+        # select words from list += 8 letters
+  
+#open the file and put it into a format that Python can understand
 with open('words.txt') as file:
     words = file.read().splitlines() 
 
-    print(words)
+    # print(words)
 
+#main area of the game: this will be the main function to call the game overall
+if __name__ == "__main__":
+    level = input([easy, medium, false])      
+#sort words into different lengths to be "called by level when" list is accessed
+#get level from user
+#get word from list based on the level
+#get guesses from the user
+#start function
