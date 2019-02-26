b47f1795a90530325079dd759e214ff48e644b26
Craig Brunengraber <brunengraber@hotmail.com>
2019-02-25 22:37:40 -0500

    basic loop

--- testing_ideas.py
+++ testing_ideas.py
@@ -1,43 +1,67 @@
-import random
-import string
+# import random
+# import string
 
 
-#get random words into game
-def auto_selected_word(words):          
-    return random.choice(words).lower()
+# def normalize_text(text):
+#     """Takes text and removes punctuation and replaces whitespace with normal spaces- compressing single spaces"""
+#     text = text.lower()
+#     valid_chars = string.ascii_letters + string.whitespace + \
+#         string.digits  # check for valid chars add them to new var
 
-def normalize_text(text):
-    """Takes text and removes punctuation and replaces whitespace with normal spaces- compressing single spaces"""
-    text = text.lower()
-    valid_chars = string.ascii_letters + string.whitespace + \
-        string.digits  # check for valid chars add them to new var
-
-    # remove punctuation
-    new_text = ""
-    for char in text:
-        if char in valid_chars:
-            new_text += char
-    text = new_text
-    text = text.replace("\n", " ")
-    return text
-
-def difficulty(words, difficulty):  # IF STATEMENT - CONDITION IS MET THAN -----X HAPPENS, NOT A REPEATING LOOP
-    if difficulty = "easy": 
-        # select words from list 4-6 letters       
-    if difficulty = "normal":
-        # select words from list 6-8 letters
-    if difficulty = "hard":
-        # select words from list += 8 letters
+#     # remove punctuation
+#     new_text = ""
+#     for char in text:
+#         if char in valid_chars:
+#             new_text += char
+#     text = new_text
+#     text = text.replace("\n", " ")
+#     return text   
   
 #open the file and put it into a format that Python can understand
-with open('words.txt') as file:
-    words = file.read().splitlines() 
+# with open('words.txt') as file:
+#     words = file.read().splitlines() 
 
     # print(words)
 
-#main area of the game: this will be the main function to call the game overall
+word_to_guess = "racecar"
+letters_guessed = []
+
+def word_guessed(word_to_guess, letters_guessed):
+    for letter in word_to_guess:
+        if letter not in letters_guessed:
+            return False
+        return True    
+
+def is_letter_input(user_input):
+    return len(user_input) == 1 and user_input.isalpha()
+
+def get_letter_input():
+    user_input = input('Type a letter to guess: ').casefold()
+    while not is_letter_input(user_input):
+        print("That was not a letter.")    
+        user_input = input('Type a letter to guess: ').casefold()
+
+    return user_input
+
+
 if __name__ == "__main__":
-    level = input([easy, medium, false])      
+    while not word_guessed(word_to_guess, letters_guessed):
+        letter_guessed = get_letter_input()
+        letters_guessed.append(letter_guessed)
+     
+
+
+
+
+
+
+#get random words into game
+# def auto_selected_word(words):          
+#     return random.choice(words).lower()    
+
+#main area of the game: this will be the main function to call the game overall
+
+
 #sort words into different lengths to be "called by level when" list is accessed
 #get level from user
 #get word from list based on the level
