1604944a338d7ca6b141579ca4a99649906833be
Craig Brunengraber <brunengraber@hotmail.com>
2019-02-25 23:32:28 -0500

    some of it works

--- testing_ideas.py
+++ testing_ideas.py
@@ -1,29 +1,29 @@
-# import random
-# import string
+import random
+import string
 
 
-# def normalize_text(text):
-#     """Takes text and removes punctuation and replaces whitespace with normal spaces- compressing single spaces"""
-#     text = text.lower()
-#     valid_chars = string.ascii_letters + string.whitespace + \
-#         string.digits  # check for valid chars add them to new var
+def normalize_text(text):
+    """Takes text and removes punctuation and replaces whitespace with normal spaces- compressing single spaces"""
+    text = text.lower()
+    valid_chars = string.ascii_letters + string.whitespace + \
+        string.digits  # check for valid chars add them to new var
 
-#     # remove punctuation
-#     new_text = ""
-#     for char in text:
-#         if char in valid_chars:
-#             new_text += char
-#     text = new_text
-#     text = text.replace("\n", " ")
-#     return text   
+    # remove punctuation
+    new_text = ""
+    for char in text:
+        if char in valid_chars:
+            new_text += char
+    text = new_text
+    text = text.replace("\n", " ")
+    return text   
   
 #open the file and put it into a format that Python can understand
-# with open('words.txt') as file:
-#     words = file.read().splitlines() 
+with open('words.txt') as file:
+    words = file.read().splitlines() 
 
     # print(words)
 
-word_to_guess = "racecar"
+word_to_guess = (words)
 letters_guessed = []
 
 def word_guessed(word_to_guess, letters_guessed):
@@ -43,11 +43,29 @@ def get_letter_input():
 
     return user_input
 
+def get_display_word(word, letters_to_show):
+    output_chars = []
+    for letter in word:
+        if letter in letters_guessed:
+            output_chars.append(letter.upper()) #why do we want upper if everything else is .lower
+        else:
+            output_chars.append("_")
+    return " ".join(output_chars)
+
+def get_word_list():
+    return(words)
+
 
 if __name__ == "__main__":
+    word_to_guess = random.choice(get_word_list())
+
+
     while not word_guessed(word_to_guess, letters_guessed):
         letter_guessed = get_letter_input()
         letters_guessed.append(letter_guessed)
+
+    print(get_display_word(word_to_guess, letters_guessed))
+    print("You won!")
      
 
 
